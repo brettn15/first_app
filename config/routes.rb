d4415b9ee2a78379f3f324d3f5ae715092524ec7
brettn15 <nowak.brett@gmail.com>
2010-08-04 00:02:49 -0400

    Finished first out of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
   map.root :controller => 'pages', :action => 'home'
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
@@ -6,5 +7,6 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id.:format'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.signup '/signup',   :controller => 'users', :action => 'new'
+  
   
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should reject email addresses identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+end

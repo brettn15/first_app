d5f72c78e2e1661f8f965d7164da381aef5e0a3d
brettn15 <nowak.brett@gmail.com>
2010-08-08 14:49:07 -0400

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,45 +3,26 @@ require 'spec_helper'
 describe UsersController do
   integrate_views
 
-  describe "GET 'show'" do
+describe "POST 'create'" do
 
-    before(:each) do
-      @user = Factory(:user)
-      # Arrange for User.find(params[:id]) to find the right user.
-      User.stub!(:find, @user.id).and_return(@user)
-    end
+    describe "success" do
 
-    it "should be successful" do
-      get :show, :id => @user
-      response.should be_success
-    end
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
 
-  it "should have the right title" do
-      get :show, :id => @user
-      response.should have_tag("title", /#{@user.name}/)
-    end
-
-    it "should include the user's name" do
-      get :show, :id => @user
-      response.should have_tag("h2", /#{@user.name}/)
-    end
-
-    it "should have a profile image" do
-      get :show, :id => @user
-      response.should have_tag("h2>img", :class => "gravatar")
-    end
-  end
-  
-  describe "GET 'new'" do
-    
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'new'
-      response.should have_tag("title", /Sign up/)
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end    
     end
   end
 end
